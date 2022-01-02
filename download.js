3e0bcf71723f2f56c722a332bc2981c926003c15
Hehly <1830904228@qq.com>
2022-01-02 12:54:15 +0800

    添加下载的方法

--- download.js
+++ download.js
@@ -15,4 +15,19 @@ export function downloadFile(fileurl, filename) {
     // 然后移除
     document.body.removeChild(a);
   }
-  
+
+
+//   多文件下载
+export function loadFilesWin(urls) {
+  var urlsArray = urls.split(",");  //被逗号分隔的多个文件的地址
+  for (var i = 0; i < urlsArray.length; i++)
+  {
+    var aDom = document.createElement("a");//创建标签
+    var evt = document.createEvent("mouseevents");//创建事件
+    evt.initMouseEvent("click", false, false);//初始化事件，绑定点击事件，不冒泡，不阻止浏览器默认行为
+    aDom.download = `file${i + 1}.${urlsArray[i].split(".")[1]}`;
+    aDom.href = urlsArray[i];//对地址进行编码
+    aDom.click();
+    aDom.dispatchEvent(evt);//触发事件
+  }
+}
