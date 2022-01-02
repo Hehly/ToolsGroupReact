301f73c9d896c6e1b234aaf3571ee8d1352a33d7
Hehly <1830904228@qq.com>
2022-01-02 12:01:19 +0800

    添加svg

--- svgIcon.tsx
+++ svgIcon.tsx
@@ -1,9 +1,20 @@
-// svg 的制作
 
-export function SvgIcon({}) {
-  return {
-    <svg>
-    <use></use>
+
+import * as React from "react";
+
+
+export
+interface Icon_t {
+  type: "water_meter" | "CombinedShape1" | "work_order" | "abnormal" |
+        "water_query_selectio" | "operation_maintenance";
+}
+
+type SvgIcon_t = React.SVGProps<SVGSVGElement> & Icon_t;
+export
+function SvgIcon({ type, style, className, }: SvgIcon_t) {
+  return (
+    <svg aria-hidden="true" style={style} className={className}>
+      <use xlinkHref={`#${type}`} />
     </svg>
-  }
+  );
 }
