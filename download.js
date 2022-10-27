eaf2a857347d502d56767f1e3ef05e6d1fced216
Hehly <1830904228@qq.com>
2022-10-27 21:13:22 +0800

    格式化代码

--- download.js
+++ download.js
@@ -1,33 +1,31 @@
 export function downloadFile(fileurl, filename) {
-    //创建隐藏的可下载链接
-    const a = document.createElement("a");
-    a.download = filename;
-    a.style.display = "none";
-    if (filename) {
-      a.href = fileurl + `?exportName=${filename}`;
-    } else {
-      a.href = fileurl;
-    }
-    //
-    document.body.appendChild(a);
-    // 触发点击
-    a.click();
-    // 然后移除
-    document.body.removeChild(a);
+  //创建隐藏的可下载链接
+  const a = document.createElement("a");
+  a.download = filename;
+  a.style.display = "none";
+  if (filename) {
+    a.href = fileurl + `?exportName=${filename}`;
+  } else {
+    a.href = fileurl;
   }
-
+  //
+  document.body.appendChild(a);
+  // 触发点击
+  a.click();
+  // 然后移除
+  document.body.removeChild(a);
+}
 
 //   多文件下载
 export function loadFilesWin(urls) {
-  var urlsArray = urls.split(",");  //被逗号分隔的多个文件的地址
-  for (var i = 0; i < urlsArray.length; i++)
-  {
-    var aDom = document.createElement("a");//创建标签
-    var evt = document.createEvent("mouseevents");//创建事件
-    evt.initMouseEvent("click", false, false);//初始化事件，绑定点击事件，不冒泡，不阻止浏览器默认行为
+  var urlsArray = urls.split(","); //被逗号分隔的多个文件的地址
+  for (var i = 0; i < urlsArray.length; i++) {
+    var aDom = document.createElement("a"); //创建标签
+    var evt = document.createEvent("mouseevents"); //创建事件
+    evt.initMouseEvent("click", false, false); //初始化事件，绑定点击事件，不冒泡，不阻止浏览器默认行为
     aDom.download = `file${i + 1}.${urlsArray[i].split(".")[1]}`;
-    aDom.href = urlsArray[i];//对地址进行编码
+    aDom.href = urlsArray[i]; //对地址进行编码
     aDom.click();
-    aDom.dispatchEvent(evt);//触发事件
+    aDom.dispatchEvent(evt); //触发事件
   }
 }
--- setCookies.tsx
+++ setCookies.tsx
@@ -1,8 +1,8 @@
 // 设置cookie
-import moment from "moment";
-import { Cookies } from "react-cookie";
+// import moment from "moment";
+// import { Cookies } from "react-cookie";
 
-// 设置一天的cookie
+// 设置一天的cookie // 设置一天时间
 export function set_cookies(token: string, account: string, expires: Date = moment().add(1, "days").toDate()) {
     if (account) new Cookies().set("account", account, { expires, path: "/" });
     new Cookies().set("lb_token", token, { expires, path: "/" });
